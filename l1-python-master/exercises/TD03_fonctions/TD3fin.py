c8b2ac4da40a6482b5bc14bdc8ced249833c5b85
uvsq22003721 <laury.kermorgant@ens.uvsq.fr>
2020-11-10 14:48:42 +0100

    10/11/20

--- l1-python-master/exercises/TD03_fonctions/TD3fin.py
+++ l1-python-master/exercises/TD03_fonctions/TD3fin.py
@@ -51,32 +51,75 @@ afficheTemps(demandeTemps())
 def sommeTemps(temps1,temps2):
     return secondeEnTemps(tempsEnSeconde(temps1) + tempsEnSeconde(temps2))
 
-sommeTemps((2,3,4,25),(5,22,57,1))
+print(sommeTemps((2,3,4,25),(5,22,57,1)))
 
-def proportionTemps(temps ,proportion):
-     return proportionTemps(proportion, temps)
-afficheTemps(proportionTemps((2 ,0 ,36 ,0) ,0.2))
+def proportionTemps(temps,proportion):
+    return secondeEnTemps(int(tempsEnSeconde(temps)*proportion))
+afficheTemps(proportionTemps((2,0,36,0),0.2))
 #appeler la fonction en échangeant l'ordre des arguments
 
-
+import time
 def tempsEnDate(temps):
-    minute = seconde // 60
-    seconde %= 60
-    heure = minute // 60
-    minute %= 60
-    jour = heure //24
-    heure %= 24
-    année = jour // 365
+    jour, heure,minute, seconde = temps
+    annee = 1970 + jour // 365
     jour %= 365
-    return (année, jour, heure, minute, seconde)
-print(temps[0],"année",temps[1],"jours",temps[2],"heures",temps[3],"minutes", temps[4],"secondes")
-
+    return (annee, jour, heure, minute, seconde)
 
 def afficheDate(date = -1):
-    pass
-
+    if date == -1:
+        date = tempsEnDate(secondeEnTemps(int(time.time())))
+    annee, jour, heure, minute, seconde = date
+    print("Année", annee, end = " ") 
+    afficheTemps((jour%365, heure, minute, seconde))
 
 temps = secondeEnTemps(1000000000)
 afficheTemps(temps)
 afficheDate(tempsEnDate(temps))
 afficheDate()
+
+def bisextile(jour):
+    annee = 1970 
+    while(jour>=8):
+        if(annee % 4 == 0 and (annee % 100 != 0 or annee % 400 == 0)):
+            print("Année", annee, "bisextile")
+            jour-=366
+        else:
+            jour-=365
+        annee +=1        
+bisextile(20000)
+
+def nombreBisextile(jour):
+    annee = 1970
+    b = 0
+    while(jour >= 0):
+        if(annee % 4 == 0 and (annee % 100 != 0 or annee % 400 == 0)):
+            b+=1
+            jour -=366
+        else:
+            jour-=365
+        annee += 1 
+    return b
+
+def tempsEnDateBisextile(temps):
+    jour, heure, minute, seconde = temps
+    return tempsEnDate((jour - nombreBisextile(jour), heure, minute, seconde))
+   
+temps = secondeEnTemps(1000000000)
+afficheTemps(temps)
+afficheDate(tempsEnDateBisextile(temps))
+
+def verifie(liste_temps):
+    if(len(liste_temps)!=4):
+        print("Liste mal formée")
+        return False
+    temps_total = 0
+    for elem in liste_temps:
+        if(tempsEnSeconde(elem)>tempsEnSeconde((0,48,0,0))):
+            return False
+        temps_total += tempsEnSeconde(elem)
+    return temps_total <= tempsEnSeconde((0,140,0,0))
+
+
+liste_temps = [[1,2,39,34],[0,1,9,4],[0,29,39,51],[0,31,13,46]]
+print(verifie(liste_temps))
+
